40f9e33c454376fc52dda57d60e3b2dd0f52790e
snil4 <snil4@walla.co.il>
2024-03-13 08:30:51 +0200

    New additions
    new view menuBar and more comments

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var rect_node = get_node("MenuBar/ColorRect")
+@onready var rect_node = $"MenuBar/ColorRect"
+@onready var touch_node = $TouchButtons
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	change_window()
@@ -11,6 +12,11 @@ func _ready():
 func _process(_delta):
 	pass
 
+
 func change_window():
 	size = get_window().get_size_with_decorations()
 	rect_node.set_size(Vector2(size.x, 41.0))
+
+
+func set_touch_mode(state: bool):
+	touch_node.visible = state
---
+++ Scripts/ViewMenu.gd
@@ -0,0 +1,21 @@
+extends MenuButton
+
+var touchMode = false
+
+@onready var about_node = get_node("/root/Main/AboutDialog")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.get_popup().connect("id_pressed", _on_item_pressed)
+	if OS.get_model_name() != "GenericDevice":
+		_on_item_pressed(0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta):
+# 	pass
+
+
+func _on_item_pressed(id):
+	match id:
+		0:
+			touchMode = not(touchMode)
+			get_popup().set_item_checked(id,touchMode)
